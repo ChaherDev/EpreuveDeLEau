0299595bc05d06f266a3a8ae07613a692b87caba
Chaher Machhour <chaher.croissance.public@gmail.com>
2024-10-19 22:13:45 +0200

    Defi reussi

--- eau00.swift
+++ eau00.swift
@@ -1,12 +1,27 @@
+// Combinaisons de 3 chiffres
+
+//Créez un programme qui affiche toutes les différentes combinaisons possibles de trois chiffres dans l’ordre croissant, dans l’ordre croissant. La répétition est volontaire.
+//
+//
+//Exemples d’utilisation :
+//$> python exo.py
+//012, 013, 014, 015, 016, 017, 018, 019, 023, 024, ... , 789
+//$>
+//
+//987 n’est pas là parce que 789 est présent.
+//
+//020 n’est pas là parce que 0 apparaît deux fois.
+//
+//021 n’est pas là parce que 012 est présent.
+//
+//000 n’est pas là parce que cette combinaison ne comporte pas exclusivement des chiffres différents les uns des autres.
+
 for i in 0...9 {
-    for j in 1...9 {
-        for k in 2...9 {
-            if i == j || j == k || i == k {
-                continue
+    for j in 0...9 {
+        for k in 0...9 {
+            if i != j && j != k && i != k && i < j && j < k {
+                print("\(i)\(j)\(k)", terminator: ", ")
             }
-            print("\(i)\(j)\(k)", terminator: ", ")
         }
     }
 }
-
-// ordre respecté, mais les combines de chiffres ne sont pas uniques ...
