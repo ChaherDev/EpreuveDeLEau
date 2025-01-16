fced96edb65f94cd060850c7d5687a161c98b81e
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-16 20:26:27 +0100

    eau01.swift completed

--- eau01.swift
+++ eau01.swift
@@ -1,21 +1,15 @@
+import Foundation
 
-func eau01() {
-    for i in 0...9 {
-        for j in 0...9 {
-            for k in 0...9 {
-                for l in 0...9 {
-                    if "\(i)\(j)" == "\(k)\(l)" {
-                        continue
-                    } else if i == 9 && j == 8 && k == 9 && l == 9 {
-                        print("\(i)\(j) \(k)\(l)")
-                        return
-                    } else {
-                        print("\(i)\(j) \(k)\(l)", terminator: ", ")
-                    }
-                }
+for i in 0...98 {
+    for j in i+1...99 {
+        if i < j {
+            if i == 98 && j == 99 {
+                print("98 99")
+            } else {
+                let formattedI = String(format: "%02d", i)
+                let formattedJ = String(format: "%02d", j)
+                print("\(formattedI) \(formattedJ)", terminator: ", ")
             }
         }
     }
 }
-
-eau01()
