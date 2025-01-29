2f332fbe91aef199b29b72bda6291b2043677bc0
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-29 17:16:45 +0100

    eau07.swift completed

--- eau07.swift
+++ eau07.swift
@@ -8,6 +8,7 @@ func uppercase (_ string: String) -> String {
             arrayOfString[letterIndex] = Character(String(arrayOfString[letterIndex]).uppercased())
             letterIndex += 1
             while arrayOfString[letterIndex].isWhitespace == false && letterIndex < arrayOfString.count {
+                arrayOfString[letterIndex] = Character(String(arrayOfString[letterIndex]).lowercased())
                 letterIndex += 1
             }
         } else {
@@ -20,7 +21,7 @@ func uppercase (_ string: String) -> String {
 
 if CommandLine.arguments.count != 2 {
     print("Vous devez fournir un argument.")
-} else if Double(CommandLine.arguments[1]) != nil {
+} else if CommandLine.arguments[1].allSatisfy({ !$0.isLetter }) {
     print("Vous devez fournir une chaîne de caractère et non un nombre.")
 } else {
     print("\(uppercase(CommandLine.arguments[1]))")
