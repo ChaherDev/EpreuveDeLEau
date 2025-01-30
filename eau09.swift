a32741b530530a6c1d11723c51d00a6f0ee40961
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-30 13:46:01 +0100

    eau09.swift completed

--- eau09.swift
+++ eau09.swift
@@ -3,22 +3,24 @@ if CommandLine.arguments.count != 3 {
 } else if Int(CommandLine.arguments[1]) == nil || Int(CommandLine.arguments[2]) == nil {
     print("error")
 } else {
-    minMax(firstArgument: Int(CommandLine.arguments[1])!, secondArgument: Int(CommandLine.arguments[2])!)
+    minMax(firstArgument: CommandLine.arguments[1], secondArgument: CommandLine.arguments[2])
 }
 
-func minMax(firstArgument: Int, secondArgument: Int) {
-    if firstArgument < secondArgument {
-        var arrayOfNumbers = [Int]
-        for i in firstArgument...secondArgument {
-            arrayOfNumbers.append(i)
+func minMax(firstArgument: String, secondArgument: String) {
+    if Int(firstArgument)! < Int(secondArgument)! {
+        var arrayOfNumbers = [String]()
+        for i in Int(firstArgument)!...Int(secondArgument)! {
+            arrayOfNumbers.append(String(i))
         }
-        let numbers = arrayOfNumbers.map { String($0) }.joined(separator: ", ")
-    } else if firstArgument > secondArgument {
-        var arrayOfNumbers = [Int]
-        for i in secondArgument...firstArgument {
-            arrayOfNumbers.append(i)
+        let numbers = arrayOfNumbers.joined(separator: ", ")
+        print(numbers)
+    } else if Int(firstArgument)! > Int(secondArgument)! {
+        var arrayOfNumbers = [String]()
+        for i in Int(secondArgument)!...Int(firstArgument)! {
+            arrayOfNumbers.append(String(i))
         }
-        let numbers = arrayOfNumbers.map { String($0) }.joined(separator: ", ") 
+        let numbers = arrayOfNumbers.joined(separator: ", ") 
+        print(numbers)
     } else {
         print (firstArgument)
     }
