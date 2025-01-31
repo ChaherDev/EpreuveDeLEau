fb55233f3e57ea38384cece448374812ac2417f7
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-01-31 10:36:34 +0100

    eau10.swift completed

---
+++ eau10.swift
@@ -0,0 +1,26 @@
+if CommandLine.arguments.count < 3 {
+    print("error")
+} else {
+    print("\(indexWanted())")
+}
+
+func indexWanted() -> Int {
+    var arguments = CommandLine.arguments
+    arguments.remove(at: 0)
+    let lastArgument = arguments.remove(at: arguments.count - 1)
+
+    if arguments.count == 1 {
+        if arguments[0] == lastArgument {
+            return 0
+        }
+    } else {
+        for i in 0...arguments.count - 1 {
+            if arguments[i] == lastArgument {
+                return i
+            }
+        }
+    }
+
+    
+    return -1
+}
