aebeb73eba02059d9c1910ed04b9dc6d42c7e1b3
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-02-02 16:55:52 +0100

    eau03.swift completed

--- eau03.swift
+++ eau03.swift
@@ -1,37 +1,35 @@
+func printTheTerm() {
+    func fibonacciSequence() {
+        thirdNumber = firstNumber + secondNumber
+        firstNumber = secondNumber
+        secondNumber = thirdNumber
+    }
+
+    while initialTerm < terminalTerm {
+        fibonacciSequence()
+        initialTerm += 1
+    }
+
+    print(firstNumber)
+}
+
 var firstNumber = 0
-var secoundNumber = 1
+var secondNumber = 1
 var thirdNumber = 0
 var initialTerm = 0
 var terminalTerm = 0
 
 if CommandLine.arguments.count != 2 {
     print("Erreur: Vous devez fournir un argument.")
-} else if Int(CommandLine.arguments[1]) != nil {
-    if Int(CommandLine.arguments[1]) == 0 {
-        print("Erreur: comment voulez-vous que je te donne le 0ème élément de la suite de Fibonacci ?")
-    }
-    if Int(CommandLine.arguments[1])! < 0 {
+} else if let input = Int(CommandLine.arguments[1]) {
+    if input < 0 {
         print("Erreur: l’index doit être un entier positif.")
+    } else if input == 0 {
+        print("Erreur: comment voulez-vous que je te donne le 0ème élément de la suite de Fibonacci ?")
     } else {
-        terminalTerm = Int(CommandLine.arguments[1])!
+        terminalTerm = input
         printTheTerm()
     }
 } else {
     print("Erreur: vous devez fournir un nombre entier.")
-}
-
-func printTheTerm() {
-    func fibonnacySequence() {
-    thirdNumber = firstNumber + secoundNumber
-    initialTerm += 1
-    if initialTerm == terminalTerm {
-        print(firstNumber)
-    }
-    firstNumber = secoundNumber
-    secoundNumber = thirdNumber
-    }
-
-    while initialTerm != terminalTerm  {
-        fibonnacySequence()
-    }
 }
