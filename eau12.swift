6ff5fef4975e41ff64219e311c322ac1f7174806
Chaher Machhour <chaher.croissance.public@gmail.com>
2025-02-02 16:14:19 +0100

    eau12.swift completed

---
+++ eau12.swift
@@ -0,0 +1,31 @@
+var arguments = CommandLine.arguments
+arguments.removeFirst()
+
+if arguments.count < 2 {
+    print("error")
+} else if areArgumentsIntegers() == false {
+    print("error")
+} else {
+    print(myBubbleSort(arguments).joined(separator: ", "))
+}
+
+func areArgumentsIntegers() -> Bool {
+    let integers = arguments.compactMap { Int($0) }
+    if arguments.count != integers.count {
+        return false
+    }
+    return true
+}
+
+func myBubbleSort(_ array: [String]) -> [String] {
+    var arrayOfInt = array.compactMap { Int($0) }
+    for _ in 0..<arrayOfInt.count {
+        for i in 0..<arrayOfInt.count-1 {
+            if arrayOfInt[i] > arrayOfInt[i+1] {
+                arrayOfInt.swapAt(i, i+1)
+            }
+        }
+    }
+    let arrayOfString = arrayOfInt.map { String($0) }
+    return arrayOfString
+}
